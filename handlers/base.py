6c39984cc436bd4f04e88db4bf69a552ed0bd029
Leonard Lin <lhl@randomfoo.net>
2015-03-11 22:18:10 -0700

    properties and stuff

--- handlers/base.py
+++ handlers/base.py
@@ -1,12 +1,43 @@
+import memcache
+from   pycket.session import SessionMixin
 from   tornado.escape import json_decode, json_encode
 import tornado.web
 
 
-class BaseHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler, SessionMixin):
   COOKIE_NAME = 'upcoming'
+
+
+  ### Session
+  @property
+  def mc(self):
+    try:
+      return self._mc
+    except:
+      self._mc = memcache.Client(['127.0.0.1:11211'])
+      return self._mc
+
+
+  ### User
+  @property
+  def user(self):
+    return self.get_current_user()
 
   def get_current_user(self):
     user_json = self.get_secure_cookie(self.COOKIE_NAME)
     if not user_json:
       return None
     return json_decode(user_json)
+  
+
+  def render(self, template_name, **kwargs):
+    if not kwargs.has_key('user'):
+      kwargs['user'] = {
+        'screen_name': 'nouser!',
+        'name': 'noname',
+        'description': '',
+        'profile_image_url': 'https://pbs.twimg.com/profile_images/458794021528158209/9VM3pPXW_normal.jpeg',
+      }
+
+    super(BaseHandler, self).render(template_name, **kwargs)
+
