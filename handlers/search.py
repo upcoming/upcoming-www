2811240adf9d1d2acae9d8b128fed8a1ed7ede2c
Andy Baio <andy@waxy.org>
2015-03-13 17:18:14 -0700

    Fix temp search handler and template

--- handlers/search.py
+++ handlers/search.py
@@ -6,10 +6,16 @@ import tornado.web
 class SearchHandler(BaseHandler, tornado.web.RequestHandler):
   def get(self, event_id=None):
     current_user = self.get_current_user()
-    events = r.table("events").order_by(index='start_date').limit(100).run()
+    events = r.table("events").order_by(
+        index='start_date'
+      ).eq_join(
+        "venue_id", r.table("venues"), index='venue_id'
+      ).zip().filter( 
+        lambda event: (event["start_date"] >= r.now())
+      ).limit(20).run()
 
     self.render(
-      "search.html",
+      "list.html",
       events = events,
       current_user = current_user
     )
