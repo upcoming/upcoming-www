d4d9041b1463677ccab79f853282ba62645ef8ea
Andy Baio <andy@waxy.org>
2015-03-24 18:12:04 -0700

    Add creator info to homepage listing

--- handlers/main.py
+++ handlers/main.py
@@ -6,13 +6,15 @@ import tornado.web
 class MainHandler(BaseHandler, tornado.web.RequestHandler):
   def get(self):
     current_user = self.get_current_user()
-    events = r.table("event").order_by(
+    events = r.table('event').order_by(
         index='start_date'
+      ).filter( 
+        lambda event: (event['start_date'] >= r.now())
       ).eq_join(
-        "venue_id", r.table("venue"), index='venue_id'
-      ).zip().filter( 
-        lambda event: (event["start_date"] >= r.now())
-      ).limit(20).run()
+        'creator_user_id', r.table("user")
+      ).without({'right': {'description': True}}).zip().eq_join(
+        'venue_id', r.table('venue'), index='venue_id'
+      ).zip().limit(20).run()
 
     self.render(
       "list.html",
