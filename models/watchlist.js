9e87c931ef1daba08a3654b0f82a77057b2c9a60
lhl <lhl@randomfoo.net>
2016-06-03 15:27:13 -0700

    deal w/ duplicate keys

--- models/watchlist.js
+++ models/watchlist.js
@@ -2,15 +2,13 @@ var db = require('../db.js');
 
 /* create a watchlist */
 exports.create = function(user, watchlist, next) {
-  var create = {
+  var post = {
     user_id: user.id,
     event_id: watchlist.event_id,
     status: watchlist.status,
   };
-
-  var update = create;
   
-  db.query('INSERT INTO watchlist SET created_at=NOW(), ?', create, function (err, result) {
+  db.query('INSERT INTO watchlist SET created_at=NOW(), ? ON DUPLICATE KEY UPDATE status=VALUES(status)', post, function (err, result) {
     if (err) return next(err);
     next(null, post);
   });
