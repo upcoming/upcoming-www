570739dbdf6b7fc3723e48d2e269558fe8b9d70b
lhl <lhl@randomfoo.net>
2016-06-03 17:20:33 -0700

    Watchlists now actually work

--- controllers/watchlists.js
+++ controllers/watchlists.js
@@ -5,19 +5,38 @@ var Watchlist = require('../models/watchlist');
 
 // POST watchlist
 router.post('/', function (req, res, next) {
-  user = req.user;
-  post = req.body;
+  var user = req.user;
+  var post = req.body;
 
-  Watchlist.create(user, post, function (err, result) {
+  if (post.status == 'attend') {
+    Watchlist.add(user, post, function (err, result) {
+      if (err) throw err;
+      if (!res.xhr) {
+        res.redirect('/event/' + post.event_id);
+      }
+    });
+  } else if (post.status == 'remove') {
+    Watchlist.remove(user, post, function (err, result) {
+      if (err) throw err;
+      if (!res.xhr) {
+        res.redirect('/event/' + post.event_id);
+      }
+    });
+  }
+});
+
+// Get watchlist item status for event/user
+router.get('/status/:event_id', function(req, res, next) {
+  var user = req.user;
+  Watchlist.get(user, req.params.event_id, function (err, result) {
     if (err) throw err;
-    res.redirect('/event/' + post.event_id);
+    res.send(result);
   });
 });
 
-
 // GET watchlists by event
 router.get('/list/:event_id', function(req, res, next) {
-  Watchlist.getAllByEvent(event_id, function (err, watchlists) {
+  Watchlist.getAllByEvent(req.params.event_id, function (err, watchlists) {
     if (err) throw err;
   });
 });
--- models/watchlist.js
+++ models/watchlist.js
@@ -1,7 +1,7 @@
 var db = require('../db.js');
 
-/* create a watchlist */
-exports.create = function(user, watchlist, next) {
+/* create a watchlist item */
+exports.add = function(user, watchlist, next) {
   var post = {
     user_id: user.id,
     event_id: watchlist.event_id,
@@ -11,6 +11,24 @@ exports.create = function(user, watchlist, next) {
   db.query('INSERT INTO watchlist SET created_at=NOW(), ? ON DUPLICATE KEY UPDATE status=VALUES(status)', post, function (err, result) {
     if (err) return next(err);
     next(null, post);
+  });
+};
+
+
+/* remove a watchlist item */
+exports.remove = function(user, watchlist, next) {
+  db.query('DELETE FROM watchlist WHERE user_id = ? AND event_id = ?', [user.id, watchlist.event_id], function (err, result) {
+    if (err) return next(err);
+    next(null, result);
+  });
+};
+
+
+/* get a watchlist item */
+exports.get = function(user, event_id, next) {
+  db.query('SELECT status, created_at FROM watchlist WHERE user_id = ? AND event_id = ?', [user.id, event_id], function (err, rows) {
+    if (err) return next(err);
+    next(null, rows[0]);
   });
 };
 
