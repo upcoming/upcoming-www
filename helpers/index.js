8d6d6fa6187b23e34514cbeb65113bcf0af207d7
Andy Baio <andy@waxy.org>
2016-06-05 11:16:05 -0700

    Remove console logging

--- helpers/index.js
+++ helpers/index.js
@@ -12,11 +12,9 @@ function getTwitterFriends(user_id, twitter_user_id, token_key, token_secret) {
   
   // save twitter following list to mysql
   twitter.get('friends/ids', { user_id: twitter_user_id },  function (err, data, response) {
-    console.log(data);
     for (var i = 0; i < data.ids.length; i++) {
       friend_twitter_id = data.ids[i];
       var post = { user_id: user_id, friend_twitter_id: friend_twitter_id };
-      console.log(post);
       var query = db.query('INSERT IGNORE INTO twitter_following SET ?', post, function (err, result) {
         if (err) throw err;
       });
@@ -30,12 +28,10 @@ function updateFollowing(user_id) {
           + "AND twitter_following.user_id = ?";
 
   var query = db.query(sql, user_id, function (err, rows) {
-    console.log(query.sql);
     if (err) throw err;
     for (var i in rows) {
       var friend = rows[i];
       var post = { user_id: user_id, friend_id: friend.friend_id, following_status: 1 };
-      console.log('friend: ' + friend.friend_id);
       db.query('INSERT IGNORE INTO following SET ?', post, function (err, result) {          
         if (err) throw err;
       });
