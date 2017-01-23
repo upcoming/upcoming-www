fb9cda054f84988a235570bc6057244fcc272401
Andy Baio <andy@waxy.org>
2017-01-22 17:52:35 -0800

    Show most recently-added friends first

--- models/friend.js
+++ models/friend.js
@@ -4,7 +4,8 @@ var db = require('../db.js');
 exports.get = function(id, next) {
   var sql = 'SELECT * FROM user, following '
     + 'WHERE user.id = following.friend_id '
-    + 'AND following.user_id = ?';
+    + 'AND following.user_id = ? '
+    + 'ORDER BY following.created_at DESC';
 
   db.query(sql, id, function (err, result) {
     if (err) return next(err);
