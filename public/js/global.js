80c6e83ac9695dd12ac169b9e074f1f678018239
Andy Baio <andy@waxy.org>
2017-02-20 12:19:25 -0800

    Remove global JS to set start date to today

--- public/js/global.js
+++ public/js/global.js
@@ -2,11 +2,4 @@ $(document).ready(function() {
   $(function(){
     $('[rel="tooltip"]').tooltip();
   });
-
-  Date.prototype.toDateInputValue = (function() {
-      var local = new Date(this);
-      local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-      return local.toJSON().slice(0,10);
-  });    
-  $('#start_date').val(new Date().toDateInputValue());
 });
