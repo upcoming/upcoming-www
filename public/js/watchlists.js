884d92d333743dfea14eb7a092343fbb7d38d6e9
Andy Baio <andy@waxy.org>
2017-03-18 01:41:20 -0700

    Remove old unused JS and debugging

--- public/js/watchlists.js
+++ public/js/watchlists.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-  // HACK: Setting Recommended Status via AJAX Call
-  // $.get('/watchlist/status/' +  $('#event_id').val(), function(data) {
-  //   if(data) {
-  //     $('#watchlist-btn').addClass('watchlist-active');
-  //     $('#status').val('remove');
-  //   }
-  // });
-
   $('input.status').change(function(e) {
     e.stopPropagation();
     
@@ -20,16 +12,8 @@ $(document).ready(function() {
       event_id: $('.event_id', form).val(), 
       status: $('input:checked', form).val()
     };
-    // alert(JSON.stringify(post));
 
     $.post('/watchlist', post, function(data) {
-      if($('this').val() == 'watch') {
-        // $('#watchlist-btn').addClass('active');
-        // $('#status').val('remove');
-      } else {
-        // $('#watchlist-btn').removeClass('active');
-        // $('#status').val('watch');
-      }
     });
 
     return false;
