3b3e2a35d1f2ab9967e2b073de48e1f35bebb26f
Andy Baio <andy@waxy.org>
2017-06-22 23:06:01 -0700

    Hide comment time from comments

--- models/comment.js
+++ models/comment.js
@@ -16,7 +16,9 @@ exports.create = function(user, comment, next) {
 
 /* find comments by event id */
 exports.getAllByEventId = function(event_id, next) {
-  var sql = 'SELECT * FROM comment, user '
+  var sql = 'SELECT comment.comment_text, comment.created_at, '
+      + 'user.name, user.avatar_image_url, user.username, comment.created_at '
+      + 'FROM comment, user '
       + 'WHERE comment.user_id = user.id '
       + 'AND comment.event_id = ?';
   db.query(sql, event_id, function (err, rows) {
