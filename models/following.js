a740d77f2b83ff99e7ae2cfdb1f0afe9665ee1c9
Andy Baio <andy@waxy.org>
2020-08-11 11:47:02 -0700

    Remove deleted users from following lists

--- models/following.js
+++ models/following.js
@@ -5,6 +5,7 @@ exports.get = function(id, next) {
   var sql = 'SELECT * FROM user, following '
     + 'WHERE user.id = following.friend_id '
     + 'AND following.user_id = ? '
+    + 'AND user.deleted = 0 '
     + 'ORDER BY following.created_at DESC';
 
   db.query(sql, id, function (err, result) {
